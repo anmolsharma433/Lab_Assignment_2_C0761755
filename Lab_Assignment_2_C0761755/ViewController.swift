cb5e1fde32bb065e0ddba09bff3076bb746154f4
anmol sharma <anmolsharma433@gmail.com>
2020-01-22 18:26:54 -0500

    Initial Commit

---
+++ Lab_Assignment_2_C0761755/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab_Assignment_2_C0761755
+//
+//  Created by Anmol Sharma on 2020-01-22.
+//  Copyright © 2020 anmol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
